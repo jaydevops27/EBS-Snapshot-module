dc58344f02d6565b299d4ba3e3d583cab6f99289
jaydevops27 <106821114+jaydevops27@users.noreply.github.com>
2024-06-10 16:16:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+resource "aws_ebs_snapshot" "my_snapshot" {
+  volume_id = var.volume_id
 
+  tags = {
+    Name        = var.snapshot_name
+    Description = var.snapshot_description
+  }
+}
+
+output "snapshot_id" {
+  description = "The ID of the created EBS snapshot"
+  value       = aws_ebs_snapshot.my_snapshot.id
+}
+
+output "snapshot_tags" {
+  description = "The tags of the created EBS snapshot"
+  value       = aws_ebs_snapshot.my_snapshot.tags
+}
