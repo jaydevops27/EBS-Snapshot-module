9f0fe6f3aa62cf9d0cf51667813168d21ff3fca8
jaydevops27 <106821114+jaydevops27@users.noreply.github.com>
2024-06-10 16:16:59 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,17 @@
+variable "volume_id" {
+  description = "The ID of the EBS volume to snapshot"
+  type        = string
+}
+
+variable "snapshot_name" {
+  description = "The name of the EBS snapshot"
+  type        = string
+  default     = "my-volume-snapshot"
+}
+
+variable "snapshot_description" {
+  description = "The description of the EBS snapshot"
+  type        = string
+  default     = "Snapshot of volume"
+}
 
